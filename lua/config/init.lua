d007a4c59bfd198992d079761fdae2c4753d3f0b
Vasily Bobnev <ardxel@MacBook-Pro-Vasily.local>
2023-12-28 20:46:17 +0300

    add configuration

---
+++ lua/config/init.lua
@@ -0,0 +1,34 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("config.globals")
+require("config.options")
+require("config.keymaps")
+
+local plugins = "plugins"
+
+local options = {
+	default = { lazy = true },
+	install = { colorscheme = { "gruvbox" } },
+	rtp = {
+		disabled_plugins = {	
+			"gzip",
+			"matchit",
+			"netrwPlugin",
+			"tarPlugin",
+			"tohtml"
+		}
+	}
+}
+
+require("lazy").setup(plugins, options)
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,4 @@
+local map = vim.keymap
+
+map.set("n", "<leader>m", ":NvimTreeFocus<CR>", {noremap = true, silent = true})
+map.set("n", "<leader>e", ":NvimTreeToggle<CR>", {noremap = true, silent = true})
